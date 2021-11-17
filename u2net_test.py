805535d8db1cddda873492e4e522c8d08e69575b
jaredyam <jihonren@gmail.com>
2021-11-17 14:12:37 +0800

    Update filename convention of generated masks

--- u2net_test.py
+++ u2net_test.py
@@ -49,7 +49,7 @@ def save_output(image_name,pred,d_dir):
     for i in range(1,len(bbb)):
         imidx = imidx + "." + bbb[i]
 
-    imo.save(d_dir+imidx+'.png')
+    imo.save(d_dir+imidx+'_mask.png')
 
 def main(image_fpath, prediction_dir, model_fpath):
 
